ae3943fbfb49186490ad3d2eafdd692e8468c9d9
Kevin Alonso Acevedo Gonzalez <acevedo19972013@gmail.com>
2023-10-03 18:15:56 -0500

    Fixed error Token VisualScripting

--- Runtime/NumericConverter.cs
+++ Runtime/NumericConverter.cs
@@ -2,7 +2,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 
 public class NumericConverter : JsonConverter
